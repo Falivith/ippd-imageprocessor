029caaf59e40c03560ae6bab7566ccaa807d5ca6
Gabriel Ramires <falivith@gmail.com>
2024-02-29 20:30:48 -0300

    Clean File

--- main.c
+++ main.c
@@ -4,59 +4,59 @@
 
 #include "util.h"
 
-// Defina o tamanho da imagem
+#define MAX_PATH_LENGTH 256
 #define IMAGE_WIDTH  800
 #define IMAGE_HEIGHT 600
 
-// Função para carregar a imagem
 void load_image(int *image, int width, int height, const char *filename) {
-    // Implemente esta função para carregar a imagem a partir de um arquivo
-    // Você pode usar bibliotecas como OpenCV ou outras para essa finalidade
 }
 
-// Função para salvar a imagem
 void save_image(int *image, int width, int height, const char *filename) {
-    // Implemente esta função para salvar a imagem em um arquivo
-    // Você pode usar bibliotecas como OpenCV ou outras para essa finalidade
 }
 
-// Função para processar a imagem
-void process_image(int *image, int width, int height) {
-    // Implemente aqui o processamento da imagem
+void process_image(int *image, int width, int height, int operation) {
 }
 
 int main(int argc, char **argv) {
     int rank, size;
+    char image_path[MAX_PATH_LENGTH];
+    int operation;
 
-    // Inicializar o MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    if (argc != 3) {
+        if (rank == 0) {
+            printf("Uso: %s <caminho_da_imagem.bmp> <operacao>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+    // Obter o caminho da imagem e a operação a ser realizada
+    strncpy(image_path, argv[1], MAX_PATH_LENGTH);
+    operation = atoi(argv[2]);
+
     // Carregar a imagem
     int *image = (int *)malloc(IMAGE_WIDTH * IMAGE_HEIGHT * sizeof(int));
     if (rank == 0) {
-        load_image(image, IMAGE_WIDTH, IMAGE_HEIGHT, "input_image.jpg");
+        load_image(image, IMAGE_WIDTH, IMAGE_HEIGHT, image_path);
     }
 
-    // Distribuir a imagem entre os processos
-    // Implemente esta parte usando MPI_Scatter
-
-    // Processar a parte da imagem em cada processo
-    process_image(image, IMAGE_WIDTH / size, IMAGE_HEIGHT);
+    process_image(image, IMAGE_WIDTH / size, IMAGE_HEIGHT, operation);
 
     // Coletar as partes processadas da imagem
     // Implemente esta parte usando MPI_Gather
 
     // Salvar a imagem resultante
     if (rank == 0) {
-        save_image(image, IMAGE_WIDTH, IMAGE_HEIGHT, "output_image.jpg");
+        save_image(image, IMAGE_WIDTH, IMAGE_HEIGHT, "output_image.bmp");
     }
 
     // Liberar memória
     free(image);
 
-    // Finalizar o MPI
     MPI_Finalize();
 
     return 0;
